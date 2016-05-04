b3c17b61739a129ea06e10335949d74ff37eae0d
noakcn <jack9855@qq.com>
2016-05-04 22:16:42 +0800

    基本项目配置

---
+++ src/main/java/com/jj/dao/IUserDao.java
@@ -0,0 +1,7 @@
+package com.jj.dao;
+
+/**
+ * Created by yewangwang on 2016/5/4.
+ */
+public interface IUserDao {
+}
---
+++ src/main/java/com/jj/filter/LoginFilter.java
@@ -0,0 +1,22 @@
+package com.jj.filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * Created by yewangwang on 2016/5/4.
+ */
+public class LoginFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+
+    public void destroy() {
+
+    }
+}
